a72718b129b0de5a4de43f30698619a570404775
gsamuk <gsamuk@gmail.com>
2022-03-16 17:49:49 +0300

    up

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,17 @@ Route::get('signout', [App\Http\Controllers\LoginController::class, 'signout'])-
 Route::middleware([Kontrol::class])->group(function () {
 
     Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
-    Route::post('profil_post', [App\Http\Controllers\UserController::class, 'profil_post'])->name('profil.post');
-    Route::post('password_change', [App\Http\Controllers\UserController::class, 'password_change'])->name('profil.password_change_post');
-    Route::post('user_authorizations', [App\Http\Controllers\AuthorizationsController::class, 'set_authorizations'])->name('user.authorizations.post');
-
+    /// user işlemleri    
+    Route::post('profil/post', [App\Http\Controllers\UserController::class, 'profil_post'])->name('profil.post');
+    Route::post('password/change', [App\Http\Controllers\UserController::class, 'password_change'])->name('profil.password_change_post');
+    Route::post('user/authorizations', [App\Http\Controllers\AuthorizationsController::class, 'set_authorizations'])->name('user.authorizations.post');
     Route::get('users', [App\Http\Controllers\UserController::class, 'user_list'])->name("users");
     Route::get('user/{id}', [App\Http\Controllers\UserController::class, 'user'])->name('user');
 
+    /// malzeme işlemleri
     Route::get('malzemeler', [App\Http\Controllers\MalzemelerController::class, 'index'])->name('malzemeler'); // Malzeme Listesi
 
-
+    // satın alma işlemleri
     Route::get('satinalma/siparis', [App\Http\Controllers\SatinAlmaController::class, 'siparis'])->name('satinalma.siparis');
     Route::get('satinalma/irsaliye', [App\Http\Controllers\SatinAlmaController::class, 'irsaliye'])->name('satinalma.irsaliye');
     Route::get('satinalma/fatura', [App\Http\Controllers\SatinAlmaController::class, 'fatura'])->name('satinalma.fatura');
